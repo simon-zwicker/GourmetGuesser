e0b7d729fafe2392a97f19c9cc4f932844a4d6e5
Simon Zwicker <github@zwicker.io>
2024-07-16 10:05:50 +0200

    Initial Commit with gitignore

---
+++ GourmetGuesser/GourmetGuesserApp.swift
@@ -0,0 +1,17 @@
+//
+//  GourmetGuesserApp.swift
+//  GourmetGuesser
+//
+//  Created by Simon Zwicker on 16.07.24.
+//
+
+import SwiftUI
+
+@main
+struct GourmetGuesserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
